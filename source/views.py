a20ffb81801a5f96af47ccf4bf7fe0133e74102b
Mohammed Hammoud <mohammed@iktw.se>
2016-02-04 22:45:10 +0100

    Add possibility to set fields

--- source/views.py
+++ source/views.py
@@ -4,18 +4,19 @@ from rest_framework.response import Response
 
 class EnumView(APIView):
     permission_classes = []
+    fields = ('i18n', )
 
     def get(self, *args, **kwargs):
         enums = self.enum_class.get_as_tuple_list()
-
         context = []
+
         for enum in enums:
             _id = enum[1]
-            i18n = self.enum_class.i18n[_id]
+            enum_context = {'id': _id}
 
-            context.append({
-                'id': _id,
-                'i18n': i18n,
-            })
+            for field in self.fields:
+                enum_context[field] = getattr(self.enum_class, field)[_id]
+
+            context.append(enum_context)
 
         return Response(context)
